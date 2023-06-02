fe1d8190917753ec7b33af51dac5e6eae01cf850
JuanPorta14 <juanporta.c@gmail.com>
2023-06-01 23:49:36 -0300

    version 1.5 de preguntas frecuentes

--- project6-preguntas-frecuentes/script.js
+++ project6-preguntas-frecuentes/script.js
@@ -1,7 +1,29 @@
-const input = document.querySelectorAll('.input');
+const plus = document.querySelectorAll('.plus');
+const question = document.querySelectorAll('.visible');
 
-input.forEach((element) => {
-    element.addEventListener("click", (event) => {
-        console.log(event.currentTarget);
-    });
+
+
+plus.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        question.forEach((elem) => {
+            
+            if (elem.classList.contains("animacion") && elem !== element.parentElement) { 
+                elem.classList.remove("animacion");
+                
+            }
+        });
+
+        plus.forEach((elem) => {
+            if (elem.classList.contains("plus-cruzado") && elem !== element) {
+                elem.classList.remove("plus-cruzado");
+            }
+        });
+        element.parentElement.classList.toggle("animacion");
+        element.classList.toggle("plus-cruzado");
+    })
+
+    
 });
+
+console.log(question);
+
